d8888cdfbd1bb40bf97e2349a13a82cd5ebea7a8
Abdelrahman Alaa Sadik <71105404+AbdelrahmanAlaa360@users.noreply.github.com>
2022-02-02 14:12:31 +0200

    Time: 0 ms (100.00%), Space: 6 MB (25.64%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/371-sum-of-two-integers.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     unsigned int getSum(unsigned int a, unsigned int b) {        
-        while(b != 0){
+        while(b){
             unsigned int temp = (a & b) << 1;
             a = a ^ b;
             b = temp;
