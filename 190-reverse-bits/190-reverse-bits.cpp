0d1f79313b336d4dd26b406fd1668483ea6a02c1
Abdelrahman Alaa Sadik <71105404+AbdelrahmanAlaa360@users.noreply.github.com>
2022-02-04 11:15:17 +0200

    Time: 4 ms (47.23%), Space: 6.1 MB (10.53%) - LeetHub

---
+++ 190-reverse-bits/190-reverse-bits.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        bitset<32>b(n);        
+        string ans = b.to_string();
+        reverse(ans.begin(), ans.end());        
+        bitset<32>b2(ans);
+        return b2.to_ulong();        
+    }
+};
