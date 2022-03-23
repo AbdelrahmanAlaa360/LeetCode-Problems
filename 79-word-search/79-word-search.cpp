a4be7a4cbbe505b877e8cefd3ccc2707c6ab999b
Abdelrahman Alaa Sadik <71105404+AbdelrahmanAlaa360@users.noreply.github.com>
2022-03-23 23:03:40 +0200

    Time: 357 ms (72.19%), Space: 8.1 MB (36.28%) - LeetHub

---
+++ 79-word-search/79-word-search.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    bool ans = false;
+    void search(vector<vector<char>>&board, int i, int j, string word, int idx) {
+        if (idx == word.size()) {
+            ans = true;
+            return;
+        }        
+        if (j + 1 < board[i].size() && board[i][j + 1] == word[idx]) {
+            char ch = board[i][j + 1];
+            board[i][j + 1] = '#';
+            search(board, i, j + 1, word, idx + 1);
+            board[i][j + 1] = ch;
+        }
+        if (j - 1 >= 0 && board[i][j - 1] == word[idx]) {
+            char ch = board[i][j - 1];
+            board[i][j - 1] = '#';
+            search(board, i, j - 1, word, idx + 1);
+            board[i][j - 1] = ch;
+        }
+        if (i + 1 < board.size() && board[i + 1][j] == word[idx]) {
+            char ch = board[i + 1][j];
+            board[i + 1][j] = '#';
+            search(board, i + 1, j, word, idx + 1);
+            board[i + 1][j] = ch;
+        }
+        if (i - 1 >= 0 && board[i - 1][j] == word[idx]) {
+            char ch = board[i - 1][j];
+            board[i - 1][j] = '#';
+            search(board, i - 1, j, word, idx + 1);
+            board[i - 1][j] = ch;
+        }
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board[i].size();j++){
+                if(board[i][j]==word[0]){
+                    char ch = board[i][j];
+                    board[i][j] = '#';
+                    search(board, i, j, word, 1);
+                    board[i][j] = ch;
+                }
+            }
+        }                           
+        return ans;
+    }
+};
