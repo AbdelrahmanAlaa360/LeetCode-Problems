d35cecf9a2cfeea2d55e97f88eecac3f7283c427
Abdelrahman Alaa Sadik <71105404+AbdelrahmanAlaa360@users.noreply.github.com>
2022-03-26 21:58:22 +0200

    Time: 7 ms (66.32%), Space: 10.4 MB (13.38%) - LeetHub

---
+++ 86-partition-list/86-partition-list.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* head1, *head2;
+    void addToList(ListNode* &temp, ListNode* &node){
+        while(temp->next!=NULL){
+            temp=temp->next;
+        }   
+        temp->next=node;
+    }
+    ListNode* partition(ListNode* head, int n) {        
+        ListNode* temp = head;
+        while (temp != NULL) {
+            ListNode* node = new ListNode;
+            node->val = temp->val;
+            node->next=NULL;
+            if (temp->val < n) {                                   
+                if(head1==NULL) head1=node;                
+                else{
+                    ListNode* temp2 = head1;
+                    addToList(temp2, node);
+                }
+            }
+            else {         
+                if(head2==NULL) head2=node;                                    
+                else{
+                    ListNode* temp2 = head2;                                        
+                    addToList(temp2, node);
+                }
+            }
+            temp = temp->next;
+        } 
+        temp = head1;
+        if(temp == NULL)return head2;
+        while(temp->next!=NULL){
+            temp=temp->next;
+        }
+        temp->next=head2;
+        return head1;
+    }
+};
