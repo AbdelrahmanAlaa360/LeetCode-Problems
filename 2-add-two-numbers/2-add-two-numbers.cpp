a67d67fb676fff5981056cea0fed6f8693606773
Abdelrahman Alaa Sadik <71105404+AbdelrahmanAlaa360@users.noreply.github.com>
2022-03-27 13:39:26 +0200

    Time: 63 ms (26.85%), Space: 72.7 MB (11.94%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -13,43 +13,44 @@ public:
     ListNode* head;
     void insertNode(int n){
         ListNode* extra = new ListNode;
-        extra->val = n;        
-        extra->next = NULL;
+        extra->val = n;                
         if(head == NULL){
             head = extra;
             return;
         }        
-        ListNode* temp = head;
-        while(temp->next != NULL){
-            temp = temp->next;
-        }
-        temp->next = extra;        
+        extra->next = head;
+        head = extra;             
     }
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         ListNode* temp1 = l1, *temp2 = l2;  
+        stack<int>res;
         int carry = 0;
         while(temp1 != NULL && temp2 != NULL){
             int sum = temp1->val + temp2->val + carry;
-            insertNode(sum % 10);            
+            res.push(sum % 10);            
             carry = sum / 10;
             temp1 = temp1->next;
             temp2 = temp2->next;
         }                
         while(temp1 != NULL){
             int sum = temp1->val + carry;
-            insertNode(sum % 10);
+            res.push(sum % 10);
             temp1 = temp1->next;
             carry = sum / 10;
         }
         while(temp2 != NULL){
             int sum = temp2->val + carry;
-            insertNode(sum % 10);
+            res.push(sum % 10);
             temp2 = temp2->next;      
             carry = sum / 10;
         }           
         if(carry){
-            insertNode(carry);
+            res.push(carry);
         }
+        while(!res.empty()){
+            insertNode(res.top());
+            res.pop();
+        }        
         return head;
     }
 };
