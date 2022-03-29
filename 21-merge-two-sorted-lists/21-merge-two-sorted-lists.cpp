aa859334019f35c2c00e53b95e05e28e4e20e01e
Abdelrahman Alaa Sadik <71105404+AbdelrahmanAlaa360@users.noreply.github.com>
2022-03-29 11:44:45 +0200

    Time: 4 ms (94.43%), Space: 15.1 MB (8.82%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -24,7 +24,7 @@ public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         ListNode *temp1 = list1, *temp2 = list2;
         while(temp1!=NULL && temp2!=NULL){
-            if(temp1->val < temp2->val){
+            if(temp1->val <= temp2->val){
                 insertNode(temp1->val);
                 temp1=temp1->next;
             }
