633ba0aecee32f02350951061b1929ca98a5c06e
Abdelrahman Alaa Sadik <71105404+AbdelrahmanAlaa360@users.noreply.github.com>
2022-04-06 15:52:13 +0200

    Time: 485 ms (12.43%), Space: 132.7 MB (11.89%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        map<int,int>mp;
-        for(int i = 1; i <= nums.size() + 1; i++)mp[i]=0;
-        for(int i = 0; i < nums.size(); i++){
-            mp[nums[i]]++;
-        }        
-        int ans = 0;
-        for(int i = 1; i <= nums.size() + 1; i++){
-            if(!mp[i]) {
-                ans = i;
-                break;
-            }
+        unordered_map<int,int>mp;
+        for(int i: nums){
+            mp[i]++;
         }
-        return ans;
+        for(int i = 1; i <= nums.size(); i++){
+            if(mp[i] == 0)return i;
+        }
+        return nums.size() + 1;
     }
 };
