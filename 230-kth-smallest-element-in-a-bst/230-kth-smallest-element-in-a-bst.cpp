a617521afb2f708fa14b7b443f8897019cecd425
Abdelrahman Alaa Sadik <71105404+AbdelrahmanAlaa360@users.noreply.github.com>
2022-04-07 17:05:42 +0200

    Time: 42 ms (7.37%), Space: 24.5 MB (16.67%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
@@ -10,8 +10,16 @@
  * };
  */
 class Solution {
-public:
-    /*vector<int>v;
+public:    
+    /*int ans = 0, k2;
+    void InorderTraversal(TreeNode* root){         
+        if(root == NULL)return;
+        InorderTraversal(root->left);
+        k2--;
+        if(k2 == 0) ans = root->val;
+        InorderTraversal(root->right);        
+    }*/
+    vector<int>v;
     void InorderTraversal(TreeNode* root, int k){         
         if(root == NULL){                        
             return;
@@ -19,21 +27,13 @@ public:
         InorderTraversal(root->left, k);        
         v.push_back(root->val);
         InorderTraversal(root->right, k);
-    } */
-    int ans = 0, k2;
-    void InorderTraversal(TreeNode* root){         
-        if(root == NULL)return;
-        InorderTraversal(root->left);
-        k2--;
-        if(k2 == 0) ans = root->val;
-        InorderTraversal(root->right);        
     }
-    int kthSmallest(TreeNode* root, int k) {        
-        k2 = k;
-        InorderTraversal(root);
-        return ans;
+    int kthSmallest(TreeNode* root, int k) {                
+        InorderTraversal(root, k);
+        return v[k - 1];
         
-        /*InorderTraversal(root, k);
-        return v[k - 1];*/
+        /*k2 = k;
+        InorderTraversal(root);
+        return ans;*/
     }    
 };
