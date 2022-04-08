fab045721ca3290d6ac6eb279e728f31f4110c72
Abdelrahman Alaa Sadik <71105404+AbdelrahmanAlaa360@users.noreply.github.com>
2022-04-08 02:16:32 +0200

    Time: 3 ms (33.98%), Space: 6.9 MB (30.82%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.cpp
+++ 54-spiral-matrix/54-spiral-matrix.cpp
@@ -8,19 +8,27 @@ public:
         return true;
     }
     void generateMatrix(vector<vector<int>>& matrix){         
-        for(int i = left; i <= right; i++) ans.push_back(matrix[top][i]);
+        for(int i = left; i <= right; i++) {
+            ans.push_back(matrix[top][i]);
+        }
         top++;
         if(!check(top, bottom, left, right))return;
-        for(int i = top; i <= bottom; i++) ans.push_back(matrix[i][right]);
+        for(int i = top; i <= bottom; i++) {
+            ans.push_back(matrix[i][right]);
+        }
         right--;
         if(!check(top, bottom, left, right))return;
-        for(int i = right; i >= left; i--) ans.push_back(matrix[bottom][i]);
+        for(int i = right; i >= left; i--) {
+            ans.push_back(matrix[bottom][i]);
+        }
         bottom--;
         if(!check(top, bottom, left, right))return;
-        for(int i = bottom; i >= top; i--) ans.push_back(matrix[i][left]);
+        for(int i = bottom; i >= top; i--) {
+            ans.push_back(matrix[i][left]);
+        }
         left++;        
         if(!check(top, bottom, left, right))return;
-        generateMatrix(matrix);  
+        generateMatrix(matrix); 
     }
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         top = 0, left = 0, bottom = matrix.size() - 1, right = matrix[0].size() - 1;       
